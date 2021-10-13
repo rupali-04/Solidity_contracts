b2c69dfcd173d19607b056b4839d92b747bfb85f
Rupali Shah <67897009+rupali-04@users.noreply.github.com>
2021-10-13 11:00:44 +0530

    Update Campaign.sol

--- Campaign.sol
+++ Campaign.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.0;
 
+ struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+
 contract Campaign{
     address public manager;
     
